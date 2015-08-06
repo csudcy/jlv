be9a305f62301a7fccc996f736822e009e56f690
Nicholas Lee <csudcy@gmail.com>
2015-08-06 20:55:23 +0100

    Initial commit

---
+++ jlv.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Jira Link Visualiser
+// @namespace    http://github.com/csudcy/jlv/
+// @version      0.1
+// @description  Visualise Jira ticket links!
+// @author       Nicholas Lee
+// @match        https://*.atlassian.net/browse/*
+// @grant        none
+// ==/UserScript==
+
+alert('hello');
